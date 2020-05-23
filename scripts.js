af231bf0799f39622172405c9f197c0759f0d15f
Daniel <danielo.visage@gmail.com>
2020-05-23 21:19:38 +0200

    initial commit

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+
+        // Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = "";
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+
+}
+
+navSlide();
+
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.querySelector(".copyright-year");
+    const currentYear = new Date().getFullYear();
+    yearSpan.textContent = currentYear;
+});
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
