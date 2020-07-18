bddfa89726eea7867d7c367b132b3c9b03665083
Daniel <danielo.visage@gmail.com>
2020-07-18 09:21:42 +0200

    Updated language selector

--- langSelect.js
+++ langSelect.js
@@ -2,24 +2,3 @@
 function langChange(el) {
   document.body.setAttribute("lang", el.value);
 }
-
-// language reload anchors
-let dataReload = document.querySelectorAll("[data-reload]");
-
-// Translations
-let langdata = {
-  eng: {
-    heading: "Hi I'm Daniel",
-    subtitle: "Front-end Developer",
-    skillsNav: "Skills",
-    portfolioNav: "Portfolio",
-    contactNav: "Contact",
-  },
-  fr: {
-    heading: "Bonjour, je suis Daniel",
-    subtitle: "Développeur frontend",
-    skillsNav: "Compétences",
-    portfolioNav: "Portfolio",
-    contactNav: "Contact",
-  },
-};
