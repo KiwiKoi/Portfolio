9778743815d915fc7eb488a116405f65e8484d40
Daniel <danielo.visage@gmail.com>
2020-07-18 09:15:22 +0200

    Added multi-language functionality

--- scripts.js
+++ scripts.js
@@ -1,24 +1,3 @@
-// language reload anchors
-let dataReload = document.querySelectorAll("[data-reload]");
-
-// Translations
-let langdata = {
-  eng: {
-    heading: "Hi I'm Daniel",
-    subtitle: "Front-end Developer",
-    skillsNav: "Skills",
-    portfolioNav: "Portfolio",
-    contactNav: "Contact",
-  },
-  fr: {
-    heading: "Bonjour, je suis Daniel",
-    subtitle: "Développeur frontend",
-    skillsNav: "Compétences",
-    portfolioNav: "Portfolio",
-    contactNav: "Contact",
-  },
-};
-
 // Automatically update copright year
 document.addEventListener("DOMContentLoaded", function () {
   const yearSpan = document.querySelector(".copyright-year");
